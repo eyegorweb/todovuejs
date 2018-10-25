0adb42ca384e39bf157cf1d1c1ae541ea03e2e47
Igor <igortridev@gmail.com>
2018-10-25 23:47:07 +0200

    toggle item to hide/show the content text

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,50 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!--<div id="nav">-->
+      <!--<router-link to="/">Home</router-link> |-->
+      <!--<router-link to="/about">About</router-link>-->
+    <!--</div>-->
+    <!--<router-view/>-->
+    <ul>
+      <Todoitem v-for="item in todos" v-bind:todoitem="item" @toggle="toggle(item)"></Todoitem>
+    </ul>
+
   </div>
 </template>
 
+<script>
+    import Todoitem from "./components/Todoitem.vue";
+    export default {
+        components: {
+            Todoitem
+        },
+
+        data(){
+            return {
+                todos: [
+                    {text: 'todo item 1', seen: true},
+                    {text: 'todo item 2', seen: true},
+                    {text: 'todo item 3', seen: false}
+                ]
+            }
+        },
+
+        methods: {
+            toggle(todo){
+                if(todo.seen){
+                    todo.seen = false;
+                }else{
+                    todo.seen = true;
+                }
+            }
+        }
+    }
+</script>
+
 <style lang="less">
+ul {
+  list-style-type: none;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Todoitem.vue
@@ -0,0 +1,24 @@
+<template>
+        <li>
+            <input type="checkbox" @click="$emit('toggle')" v-model="todoitem.seen"/>
+            <!--<input type="checkbox" @click="$emit('toggle')" v-bind:value="todoitem.seen" v-on:input="todoitem.seen = $event.target.value"/>-->
+            <span v-if="todoitem.seen">{{ todoitem.text }}</span>
+        </li>
+</template>
+
+<script>
+    export default {
+        name: 'Todoitem',
+        props: ['todoitem'], // propriétés de l'instance du composant
+        data() { // data doit être une fonction afin que chaque instance puisse conserver une copie indépendante de l'objet retourné
+            return {
+                seen: true
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
