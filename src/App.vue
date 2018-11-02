c423b3f7ed593cb1f02641e064d39bedb0b5d1a2
igor.trifunovic3@docapost.fr <igor.trifunovic3@docapost.fr>
2018-11-02 11:35:22 +0100

    refs:#todo - init and toggle

--- src/App.vue
+++ src/App.vue
@@ -5,40 +5,60 @@
       <!--<router-link to="/about">About</router-link>-->
     <!--</div>-->
     <!--<router-view/>-->
+    <input type="text" v-bind:value="text"  v-on:input="text = $event.target.value" @keyup.enter="addTodo">
+    <!--<input type="text" v-model="text" @keyup.enter="addTodo">-->
     <ul>
-      <Todoitem v-for="item in todos" v-bind:todoitem="item" @toggle="toggle(item)"></Todoitem>
+      <Todoitem v-for="(item, i) of todos" :key="item.id" :todoitem="item" @toggle="toggle" @remove="deleteTodo(todos, item.id)"></Todoitem>
+      <Todoitem v-for="(item, i) in todos2" :key="item.id" v-bind:todoitem="item" @toggle="toggle" @remove="deleteTodo(todos2, item.id)"></Todoitem>
     </ul>
+
+    <pre>{{ $data }}</pre>
 
   </div>
 </template>
 
 <script>
-    import Todoitem from "./components/Todoitem.vue";
-    export default {
-        components: {
-            Todoitem
-        },
+import Todoitem from "./components/Todoitem.vue";
+let id = 0;
+export default {
+  components: {
+    Todoitem
+  },
 
-        data(){
-            return {
-                todos: [
-                    {text: 'todo item 1', seen: true},
-                    {text: 'todo item 2', seen: true},
-                    {text: 'todo item 3', seen: false}
-                ]
-            }
-        },
+  data() {
+    return {
+      text: "Type your text",
+      todos: [
+        { text: "todo item 1", seen: true, isEditing: true, id: id++ },
+        { text: "todo item 2", seen: true, isEditing: true, id: id++ },
+        { text: "todo item 3", seen: false, isEditing: false, id: id++ }
+      ],
+      todos2: [
+        { text: "todo item 1", seen: true, isEditing: true, id: id++ },
+        { text: "todo item 2", seen: true, isEditing: true, id: id++ },
+        { text: "todo item 3", seen: false, isEditing: false, id: id++ }
+      ]
+    };
+  },
 
-        methods: {
-            toggle(todo){
-                if(todo.seen){
-                    todo.seen = false;
-                }else{
-                    todo.seen = true;
-                }
-            }
-        }
+  methods: {
+    addTodo() {
+      if (!this.text) return;
+      this.todos.push({ text: this.text, seen: true, isEditing: true, id: id++ });
+      this.text = "";
+    },
+    deleteTodo(array, id) {
+      console.log("delete todo item ", id);
+      array.splice(array.findIndex(todo => todo.id === id), 1)
     }
+  },
+
+  computed: {
+    toggle() {
+      return this.todos.filter(todo => (todo.seen ? !todo.seen : todo.seen));
+    }
+  }
+};
 </script>
 
 <style lang="less">
--- src/components/Todoitem.vue
+++ src/components/Todoitem.vue
@@ -1,8 +1,9 @@
 <template>
         <li>
             <input type="checkbox" @click="$emit('toggle')" v-model="todoitem.seen"/>
-            <!--<input type="checkbox" @click="$emit('toggle')" v-bind:value="todoitem.seen" v-on:input="todoitem.seen = $event.target.value"/>-->
+            <!--<input type="checkbox" @click="$emit('toggle')" v-bind:checked="todoitem.seen" v-on:input="todoitem.seen = $event.target.checked"/>-->
             <span v-if="todoitem.seen">{{ todoitem.text }}</span>
+            <button @click="$emit('remove')">X</button>
         </li>
 </template>
 
@@ -12,7 +13,7 @@
         props: ['todoitem'], // propriétés de l'instance du composant
         data() { // data doit être une fonction afin que chaque instance puisse conserver une copie indépendante de l'objet retourné
             return {
-                seen: true
+                toto: true
             }
         }
     }
